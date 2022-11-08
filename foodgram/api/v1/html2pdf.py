3ddddd32af5f5533db337d910ceb59cdff5a325f
Ilya Steblyan <102170248+Hrushon@users.noreply.github.com>
2022-11-08 10:53:51 +0600

    Update html2pdf.py

--- foodgram/api/v1/html2pdf.py
+++ foodgram/api/v1/html2pdf.py
@@ -3,12 +3,28 @@ from django.template.loader import get_template
 from xhtml2pdf import pisa
 
 
+def fetch_pdf_resources(uri, rel):
+    if uri.find(settings.MEDIA_URL) != -1:
+        path = os.path.join(
+            settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, '')
+        )
+    elif uri.find(settings.STATIC_URL) != -1:
+        path = os.path.join(
+            settings.STATIC_ROOT, uri.replace(settings.STATIC_URL, '')
+        )
+    else:
+        path = None
+    return path
+
+
 def html_to_pdf(template, context):
     response = HttpResponse(content_type="application/pdf")
     response["Content-Disposition"] = 'filename="shopping_cart.pdf"'
     template = get_template(template)
     html = template.render(context)
-    pdf = pisa.CreatePDF(html, dest=response, encoding="utf-16")
+    pdf = pisa.CreatePDF(
+        html, dest=response, encoding="utf-8", link_callback=fetch_pdf_resources
+    )
     if not pdf.err:
         return response
     return None
