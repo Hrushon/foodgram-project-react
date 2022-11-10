8d12caf9366f775d247d7590bade4daf28ad045d
Ilya Steblyan <102170248+Hrushon@users.noreply.github.com>
2022-11-10 11:37:08 +0600

    Update serializers.py

--- foodgram/api/v1/serializers.py
+++ foodgram/api/v1/serializers.py
@@ -2,7 +2,6 @@ import base64
 
 from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
-from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -15,34 +14,7 @@ from head.models import (
     Tag,
     TagRecipe
 )
-from users.models import User
-
-
-class CustomUserCreateSerializer(UserCreateSerializer):
-    """Сериализатор для регистрации пользователей."""
-    class Meta:
-        model = User
-        fields = ('email', 'username', 'first_name', 'last_name', 'password')
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """Сериализатор для пользователей."""
-    is_subscribed = serializers.SerializerMethodField()
-
-    class Meta:
-        model = User
-        fields = (
-            'email',
-            'id',
-            'username',
-            'first_name',
-            'last_name',
-            'is_subscribed'
-        )
-
-    def get_is_subscribed(self, obj):
-        user = self.context['request'].user
-        return bool(obj.following.filter(user=user))
+from users.serializers import UserSerializer
 
 
 class IngredientSerializer(serializers.ModelSerializer):
