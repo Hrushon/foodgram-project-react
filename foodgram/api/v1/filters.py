8a2bb9cecf6f662f684177438780047a42608dd5
Ilya Steblyan <102170248+Hrushon@users.noreply.github.com>
2022-11-15 17:04:46 +0600

    Update filters.py

--- foodgram/api/v1/filters.py
+++ foodgram/api/v1/filters.py
@@ -1,4 +1,4 @@
-from django_filters.rest_framework import (ModelMultipleChoiceFilter, FilterSet,
+from django_filters.rest_framework import (CharFilter, FilterSet,
                                            NumberFilter)
 
 from head.models import Recipe, Tag
@@ -13,17 +13,16 @@ class RecipeFilter(FilterSet):
     is_in_shopping_cart = NumberFilter(
         field_name='buyer__user', method='filter_users_lists'
     )
-    tags = ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        queryset=Tag.objects.all(),
-        to_field_name='slug'
-    )
+    tags = CharFilter(method='tags_filter')
 
     def filter_users_lists(self, queryset, name, value):
         user = self.request.user
         if user.is_anonymous or not int(value):
             return queryset
         return queryset.filter(**{name: user})
+
+    def tags_filter(self, queryset, name, value):
+        return queryset.filter(**{ name: value, }).distinct()
 
     class Meta:
         model = Recipe
